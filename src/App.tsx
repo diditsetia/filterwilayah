2ffd5660836893fc77b67df8f4ec40be68783d2d
diditsetia <didit.budi@ist.id>
2026-03-19 03:24:20 +0700

    setting app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="p-10 bg-blue-500 text-white text-2xl">
